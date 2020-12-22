e78ba0abb00553133a72021cbaf4326600d4507e
Dmitry Shestak <dimonshestak160201@gmail.com>
2020-12-22 09:06:30 +0300

    ful main page

--- app/js/common.js
+++ app/js/common.js
@@ -9,3 +9,30 @@ $('.slick-slider').slick({
    nextArrow: '<button type="button" class="slick-next"><img src="img/Icon/Dark/next.svg" alt="Next"></button>',
    prevArrow: '<button type="button" class="slick-prev"><img src="img/Icon/Dark/prev.svg" alt="Prev"></button>'
  });
+
+
+$('#feedback__slider__img').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerPadding: '0px',
+  centerMode: true,
+  arrows: false,
+  dots: false,
+  fade: false,
+  draggable: false,
+  asNavFor: '#feedback__slider__text'
+});
+$('#feedback__slider__text').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '#feedback__slider__img',
+  autoplay: true,
+  fade: true,
+  dots: true,
+  arrows: true,
+  centerMode: true,
+  draggable: false,
+  focusOnSelect: true,
+  nextArrow: '<button type="button" class="slick-next"><img src="img/Icon/Dark/next.svg" alt="Next"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/Icon/Dark/prev.svg" alt="Prev"></button>'
+});
